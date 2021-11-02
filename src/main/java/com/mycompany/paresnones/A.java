0c8777301ef0617e148f59f47199715fbd8c5472
lorena_olay <olaylorena2002@gmail.com>
2021-11-02 12:56:57 +0100

    he hecho comentarios

--- src/main/java/com/mycompany/paresnones/A.java
+++ src/main/java/com/mycompany/paresnones/A.java
@@ -16,27 +16,30 @@ public class A {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //declaramos lo que necesitamos
         Scanner entradateclado = new Scanner(System.in);
         int num1, num2, entero, resto;
         // TODO code application logic here
-        do
-        {
+        //bucle del primer número
+        do {
             System.out.println("Dame el primer número");
             num1 = entradateclado.nextInt();
-            if(!(0<num1)){
+            if (!(0 < num1)) {
                 System.out.println("solo trabajamos con números positivos");
             }
-        }while(!(0 < num1));
-        do{
+        } while (!(0 < num1));
+        //bucle del segundo número
+        do {
             System.out.println("Dame el segundo número");
             num2 = entradateclado.nextInt();
-            if(!(0<num2)){
+            if (!(0 < num2)) {
                 System.out.println("solo trabajamos con números positivos");
             }
-        }while(!(0 < num2));
-        
+        } while (!(0 < num2));
+        //operaciones para averiguar si es par o impar
         entero = num1 + num2;
         resto = entero % 2;
+        //condicion para mostrar al usuario el resultado del juego
         if (resto == 0) {
             System.out.println("pares");
         } else {
