48f30545948994b9782a70a4c355291b386fa445
lorena <olaylorena2002@gmail.com>
2021-11-02 18:26:08 +0100

    copido y pegado las modificaciones del A y puesto el numero aleatorio

--- src/main/java/com/mycompany/paresnones/B.java
+++ src/main/java/com/mycompany/paresnones/B.java
@@ -17,39 +17,68 @@ public class B {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //repositorio
+        /*https://github.com/lorena-olay/JAVA-Pares-Nones*/
         //declaramos lo que necesitamos
-
         Scanner entradateclado = new Scanner(System.in);
         int num1, num2, entero, resto;
         Random numAleatrio = new Random();
         // TODO code application logic here
-        System.out.println("Piensa el equipo que eliges, si pares o nones");
+        //bucle para controlar que no eligan el mismo equipo
+        do{
+        //elegimos equipos usuario 1
+        do {
+            System.out.println("Dime el equipo que eliges usuario 1: ");
+            System.out.println("1 - Pares");
+            System.out.println("2 - Nones");
+            equipo = entradateclado.nextInt();
+            if(!(equipo == 1 || equipo == 2)){
+                System.out.println("por favor, introduce una de las opciones");
+            }
+        } while (!(equipo == 1 || equipo == 2));
+        //elegimos equipos usuario 2
+        do {
+            System.out.println("Dime el equipo que eliges usuario 2: ");
+            System.out.println("1 - Pares");
+            System.out.println("2 - Nones");
+            equipo1 = entradateclado.nextInt();
+            if(!(equipo1 == 1 || equipo1 == 2)){
+                System.out.println("por favor, introduce una de las opciones");
+            }
+        } while (!(equipo1 == 1 || equipo1 == 2));
+        }while(equipo == equipo1);
         //bucle del primer número
         do {
-            System.out.println("Dame el primer número");
+            System.out.println("Dame el número usuario 1");
             num1 = entradateclado.nextInt();
-            if (!(0 < num1)) {
-                System.out.println("solo trabajamos con números positivos");
+            if (!(0 < num1 || 10 > num1)) {
+                System.out.println("solo trabajamos con números positivos entre el 0 y el 10");
             }
-        } while (!(0 < num1));
+        } while (!(0 < num1 || 10 > num1));
         //bucle del segundo número
         do {
-            System.out.println("Dame el segundo número");
+            System.out.println("Dame el número usuario 2");
             num2 = numAleatrio.nextInt();
-            System.out.println(num2);
-            if (!(0 < num2)) {
-                System.out.println("solo trabajamos con números positivos");
+            if (!(0 < num2 || 10 > num1)) {
+                System.out.println("solo trabajamos con números positivos entre el 0 y el 10");
             }
-        } while (!(0 < num2));
+        } while (!(0 < num2 || 10 > num2));
         //operaciones para averiguar si es par o impar
         entero = num1 + num2;
         resto = entero % 2;
         //condicion para mostrar al usuario el resultado del juego
-        if (resto == 0) {
-            System.out.println("pares");
+        if (resto == 0 && equipo == 1) {
+            System.out.println("HAS GANADO CON PARES! Muy bien usuario 1");
+        } else if (!(resto == 0) && equipo == 2) {
+            System.out.println("HAS GANADO CON NONES! Muy bien usuario 1");
+        } else if (resto == 0 && equipo1 == 1) {
+            System.out.println("HAS GANADO CON PARES! Muy bien usuario 2");
+        } else if (!(resto == 0) && equipo1 == 2) {
+            System.out.println("HAS GANADO CON NONES! Muy bien usuario 2");
         } else {
-            System.out.println("nones");
+            System.out.println("Que mal, has perdido. Ha ganado el equipo contrario");
         }
+
     }
 
 }
